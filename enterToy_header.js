cf2a8c1cc806ecfd1318c07d698cf6a12f48029f
zmapleshine <715017@163.com>
2021-02-16 23:25:05 +0800

    Create enterToy_header.js

---
+++ enterToy_header.js
@@ -0,0 +1,7 @@
+const version = v1.0
+console.log("Script Version:",version)
+const respHeader = $response.headers
+console.log("Response Header:",respHeader)
+respHeader["errorCode"] = "0"
+console.log("Fake Header:",respHeader)
+$done({ respHeader })
