9a4c839f14b18bb50e438e1b3fb0d9d8f460bbce
zmapleshine <715017@163.com>
2021-02-17 20:31:03 +0800

    Delete enterToy_header.js

--- enterToy_header.js
+++
@@ -1,7 +0,0 @@
-const version = "v1.0"
-console.log("Script Version:",version)
-const respHeader = $response.headers
-console.log("Response Header:",respHeader)
-respHeader["errorCode"] = "0"
-console.log("Fake Header:",respHeader)
-$done({ respHeader })
